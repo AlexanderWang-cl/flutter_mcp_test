11c073bd463b93a6a89ac4ad15928c7f553f4119
Alexander_Wang <Alexander_Wang@cyberlink.com>
2025-11-08 14:20:49 +0800

    Remove redudent files

--- flutter_mcp_test/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_mcp_test
-
-A new Flutter project.
--- flutter_mcp_test/android/app/src/main/kotlin/com/example/flutter_mcp_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_mcp_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
